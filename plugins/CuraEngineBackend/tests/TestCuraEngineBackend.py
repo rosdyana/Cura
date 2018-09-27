56a01edf33e6d3a20d30e1ba81ecba5eb48214c7
Remco Burema <r.burema@ultimaker.com>
2018-09-27 17:05:24 +0200

    [UnitTests] Extend colorCodeToRGBA test.

--- plugins/CuraEngineBackend/tests/TestCuraEngineBackend.py
+++ plugins/CuraEngineBackend/tests/TestCuraEngineBackend.py
@@ -3,6 +3,11 @@ import unittest.mock #For the mocking and monkeypatching functionality.
 
 from plugins.CuraEngineBackend.ProcessSlicedLayersJob import colorCodeToRGBA
 
-def test_colorCodeToRGBA():
-    assert colorCodeToRGBA("#FFFFFF") == [1., 1., 1., 1.]
-
+@pytest.mark.parametrize("color_compare", [
+    ["#FFFFFF", [1., 1., 1., 1.]],
+    ["#FF00FF", [1., 0., 1., 1.]],
+    ["#00FF00", [0., 1., 0., 1.]],
+    ["#000000", [0., 0., 0., 1.]]
+])
+def test_colorCodeToRGBA(color_compare):
+    assert colorCodeToRGBA(color_compare[0]) == color_compare[1]
